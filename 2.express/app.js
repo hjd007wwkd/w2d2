b34a18da1704d59bd6122f0d228961425f987b25
Jin <hjd007wwkd@hotmail.com>
2018-05-15 04:19:43 +0000

    intial

---
+++ 2.express/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", function(req, res){
+  res.end("Hello!");
+});
+
+app.get("/urls.json", function(req, res) {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.listen(PORT, function(){
+  console.log(`Example app listening on port ${PORT}!`);
+});
